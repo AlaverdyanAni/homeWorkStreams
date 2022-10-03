c9b6e44288ac0d150b6f77a9d84cf94243685b2b
Ani <ani-alaverdyan@mail.ru>
2022-10-03 19:58:42 +0300

    commit

--- src/main/java/com/example/homeworkstreams/EmployeeService.java
+++ src/main/java/com/example/homeworkstreams/EmployeeService.java
@@ -1,2 +1,18 @@
-package com.example.homeworkstreams;public interface EmployeeService {
-}
+package com.example.homeworkstreams;
+
+import java.util.List;
+
+    public interface EmployeeService {
+        Employee addEmployee(String fullName, int depId, int salary);
+        Employee removeEmployee (String fullName);
+        Employee findEmployee (String fullName);
+        Employee getMaxSalaryEmployee(int depId);
+        Employee getMinSalaryEmployee(int depId);
+        List<Employee> printEmployeesByDepartments();
+
+        List<Employee> printEmployeesForDepartment(int depId);
+
+        List<Employee> printEmployees();
+
+    }
+
